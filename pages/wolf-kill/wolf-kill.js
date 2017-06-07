045983182d0a803feaf161cdaeedbb4efb6e582f
jerry <1804823423@qq.com>
2017-06-07 18:16:27 +0800

    手机短信发送

---
+++ pages/wolf-kill/wolf-kill.js
@@ -0,0 +1,10 @@
+Page({
+    onReady: function (res) {
+    },
+    data: {
+        items: [
+            {name: '1', value: '标准模式',checked: 'true'},
+            {name: '2', value: '狂野模式'},
+        ]
+    },
+})
